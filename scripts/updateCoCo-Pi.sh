31c893ab7046d3fa87540f89c778ecfad68e51db
Ron Klein <ron@kdomain.org>
2021-01-05 07:49:56 -0600

    update various scripts

--- scripts/updateCoCo-Pi.sh
+++ scripts/updateCoCo-Pi.sh
@@ -5,9 +5,11 @@
 # make sure to be in /home/pi
 cd $HOME
 
-git pull origin master
+#git pull origin master
+git diff --name-only origin master
 
 echo -e
 echo -e
 read -p "Press any key to continue... " -n1 -s
 echo -e
+
